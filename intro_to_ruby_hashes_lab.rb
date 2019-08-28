0ab80ca74ed125a64231275f9869c267fefaa5de
James Batsell <jamesbatsell05@gmail.com>
2019-08-28 07:36:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,9 @@ def monopoly_with_third_tier
   railroads: {
     pieces: 4
     rent_in_dollars:{
-      one_piece_owned
+      one_piece_owned: 25
+      two_piece_owned: 50
+      three_piece_owned: 75
     }
   }
 }
